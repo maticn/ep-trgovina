4f907868a7f0d76917da9fd88c9f93981999d874
klukec <maticnova@gmail.com>
2016-01-11 18:57:27 +0100

    Spisan začetek Android app, importi ne delajo.

--- AndroidTrgovina/app/src/main/java/fri/ep/androidtrgovina/MainActivity.java
+++ AndroidTrgovina/app/src/main/java/fri/ep/androidtrgovina/MainActivity.java
@@ -5,25 +5,70 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fri.ep.androidtrgovina.model.Izdelki;
+import fri.ep.androidtrgovina.model.IzdelkiResponse;
+import fri.ep.androidtrgovina.volley.VolleyTrgovinaRequest;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String ADDRESS = "https://localhost/netbeans/trgovina/izdelkiApi/";
+
+    private static final String TAG = MainActivity.class.getCanonicalName();
+    private ListView articlesTable;
+    private Button refreshButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        articlesTable = (ListView) findViewById(R.id.articlesTable_lv);
+        refreshButton = (Button) findViewById(R.id.refresh_btn);
+        refreshButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                final RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+                final VolleyTrgovinaRequest request = new VolleyTrgovinaRequest(ADDRESS,
+                        new Response.Listener<IzdelkiResponse>() {
+                            @Override
+                            public void onResponse(IzdelkiResponse response) {
+                                final List<Izdelki> vsiIzdelki = new ArrayList<>();
+
+                                for (Izdelki izdelek : response.izdelkiList) {
+                                    vsiIzdelki.add(izdelek);
+                                }
+
+                                final ArrayAdapter<Izdelki> aa = new ArrayAdapter<>(MainActivity.this,
+                                        android.R.layout.simple_list_item_1, vsiIzdelki);
+                                articlesTable.setAdapter(aa);
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.w(TAG, " Volley error: " + error.getLocalizedMessage());
+                                articlesTable.setAdapter(null);
+                            }
+                        });
+
+                queue.add(request);
             }
         });
     }
---
+++ AndroidTrgovina/app/src/main/java/fri/ep/androidtrgovina/model/IzdelkiResponse.java
@@ -0,0 +1,11 @@
+package fri.ep.androidtrgovina.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Matic-ProBook on 11. 01. 2016.
+ */
+public class IzdelkiResponse {
+        public List<Izdelki> izdelkiList = new ArrayList<>();
+}
